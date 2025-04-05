dc9f681f8acd454c84ee667c3ed69b6882e218b3
Jhonnathan0314 <jffc314@gmail.com>
2025-04-04 19:22:30 -0500

    Upgrade mapper use in utils and code context

--- src/main/java/com/easy/store/backend/context/codes/infrastructure/mapper/CodeMapper.java
+++ src/main/java/com/easy/store/backend/context/codes/infrastructure/mapper/CodeMapper.java
@@ -3,12 +3,10 @@ package com.easy.store.backend.context.codes.infrastructure.mapper;
 import com.easy.store.backend.context.codes.application.dto.CodeDTO;
 import com.easy.store.backend.context.codes.domain.model.Code;
 import com.easy.store.backend.context.codes.infrastructure.persistence.CodeEntity;
-import com.easy.store.backend.utils.mappers.Mapper;
+import com.easy.store.backend.utils.mappers.BaseMapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
+public class CodeMapper extends BaseMapper<CodeEntity, Code, CodeDTO> {
 
-public class CodeMapper implements Mapper<CodeEntity, Code, CodeDTO> {
     @Override
     public Code entityToModel(CodeEntity entity) {
         return Code.builder()
@@ -49,31 +47,4 @@ public class CodeMapper implements Mapper<CodeEntity, Code, CodeDTO> {
                 .build();
     }
 
-    @Override
-    public List<Code> entitiesToModels(List<CodeEntity> entities) {
-        return entities.stream()
-                .map(this::entityToModel)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<CodeEntity> modelsToEntities(List<Code> models) {
-        return models.stream()
-                .map(this::modelToEntity)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<CodeDTO> modelsToDtos(List<Code> models) {
-        return models.stream()
-                .map(this::modelToDto)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Code> dtosToModels(List<CodeDTO> dtos) {
-        return dtos.stream()
-                .map(this::dtoToModel)
-                .collect(Collectors.toList());
-    }
 }
