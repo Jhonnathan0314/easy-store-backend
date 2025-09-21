3b2197d72028a2b06aee604f06611f3ef7fe9d24
Jhonnathan0314 <jffc314@gmail.com>
2025-09-20 21:39:33 -0500

    Delete unused constant.

--- src/main/java/com/easy/store/backend/utils/constants/FileConstants.java
+++ src/main/java/com/easy/store/backend/utils/constants/FileConstants.java
@@ -3,7 +3,6 @@ package com.easy.store.backend.utils.constants;
 public class FileConstants {
 
     public static final String DEFAULT_CATEGORY_IMG = "store.png";
-    public static final String CATEGORY_IMG_ASSIGNED_NAME = "1.png";
 
     public static final String DEFAULT_PRODUCT_IMG = "product.png";
 
