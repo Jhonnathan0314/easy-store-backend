ff2c0e8e6f7f999f77b901d13b851e0c93dcaca3
Jhonnathan0314 <jffc314@gmail.com>
2025-09-20 21:42:32 -0500

    Delete image from s3 when a category was deleted.

--- src/main/java/com/easy/store/backend/context/category/application/usecase/DeleteByIdCategoryUseCase.java
+++ src/main/java/com/easy/store/backend/context/category/application/usecase/DeleteByIdCategoryUseCase.java
@@ -2,7 +2,9 @@ package com.easy.store.backend.context.category.application.usecase;
 
 import com.easy.store.backend.context.category.domain.model.Category;
 import com.easy.store.backend.context.category.domain.port.CategoryRepository;
+import com.easy.store.backend.context.s3.service.S3Service;
 import com.easy.store.backend.utils.constants.ErrorMessages;
+import com.easy.store.backend.utils.constants.FileConstants;
 import com.easy.store.backend.utils.exceptions.NonExistenceException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,7 @@ import java.util.Optional;
 public class DeleteByIdCategoryUseCase {
 
     private final CategoryRepository categoryRepository;
+    private final S3Service s3Service;
 
     public void deleteById(Long id) throws NonExistenceException {
 
@@ -29,6 +32,10 @@ public class DeleteByIdCategoryUseCase {
         log.info("ACCION DELETEBYID CATEGORY -> Eliminando categoria");
 
         categoryRepository.deleteById(id);
+
+        if(!category.get().getImageName().equals(FileConstants.DEFAULT_CATEGORY_IMG)) {
+            s3Service.deleteObject(category.get().getAccount().getId(), FileConstants.CATEGORY_CONTEXT, category.get().getImageName());
+        }
     }
 
 }
